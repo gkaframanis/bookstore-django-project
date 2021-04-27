6cfcc6d67725199f558569a5346f4e6f2768606a
Grigorios Kaframanis <gkaframanis@gmail.com>
2021-04-27 14:32:59 +0300

    Use of permissions

--- books/views.py
+++ books/views.py
@@ -1,15 +1,21 @@
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.views.generic import ListView, DetailView
 from .models import Book
 
 
-class BookListView(ListView):
+class BookListView(LoginRequiredMixin, ListView):
     model = Book
     context_object_name = "book_list"
     template_name = "books/book_list.html"
+    # account_login since we use django-allauth | login for the traditional Django authentication system
+    login_url = "account_login"
 
 
-class BookDetailView(DetailView):
+class BookDetailView(LoginRequiredMixin, PermissionRequiredMixin, DetailView):
     model = Book
     context_object_name = "book"
     template_name = "books/book_detail.html"
-
+    login_url = "account_login"
+    # permission names take the form "<app label>.<permission codename>"
+    # Only the users that have this permission can the see the details of a book.
+    permission_required = "books.special_status"
